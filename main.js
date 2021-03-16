4f9e462c0df5e82affbb8af322b05c5be8f82c76
johnworker <besscomdark@gmail.com>
2021-03-16 22:27:26 +0800

    RWD調整

--- main.js
+++ main.js
@@ -5,7 +5,27 @@ $("#banner").slick({
 });
 
 $("#work").slick({
-    slidesToShow: 3,
+    slidesToShow: 4,
+    slidesToScroll: 1, 
     autoplay: true,
-    autoplaySpeed: 2000
+    autoplaySpeed: 2000,
+    // 響應式 RWD
+    responsive: [
+        // 平板：小於 1025
+        {
+            breakpoint: 1025,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+            }
+        },
+        // 手機：小於 767
+        {
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
 });
